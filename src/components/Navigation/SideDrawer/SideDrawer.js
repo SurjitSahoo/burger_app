fc3153f16466816e5719759c29b71ae50b1a0a0b
Surjit Kumar Sahoo <surjitsahoo0@gmail.com>
2019-02-25 10:21:32 +0530

    fixed auxilary

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './SideDrawer.module.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxiliary';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/BackDrop/Backdrop';
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Backdrop from '../BackDrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxiliary';
 
 class Modal extends Component {
 
